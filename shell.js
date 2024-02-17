5d90397f855dbe675fa81634f270c639fcfa465e
Sarai Sanford <railove017@gmail.com>
2024-02-17 11:59:43 -0500

    updated all

--- shell.js
+++ shell.js
@@ -3,3 +3,19 @@ console.log(mdc);
 const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar;
 const topAppBarElement = document.querySelector(".mdc-top-app-bar");
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const MDCTextField = mdc.textField.MDCTextField;
+const foos = [].map.call(
+  document.querySelectorAll(".mdc-text-field"),
+  function (el) {
+    return new MDCTextField(el);
+  }
+);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const loginIcon = document.querySelector('#loginIcon');
+
+  loginIcon.addEventListener('click', function() {
+    window.location.href = 'eligibility.html';
+  });
+});
